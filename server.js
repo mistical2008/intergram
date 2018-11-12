c02abfc029e53178f2f4542a22c7d846764f3798
Evgeniy Talko <etalko@gmail.com>
2018-11-12 11:29:13 +0300

    added site.ru to userId

--- server.js
+++ server.js
@@ -77,7 +77,14 @@ function sendTelegramMessage(chatId, text, parseMode) {
         .form({
             "chat_id": chatId,
             "text": text,
-            "parse_mode": parseMode
+            "parse_mode": parseMode,
+            reply_markup: JSON.stringify({
+                inline_keyboard: [
+                  [{ text: 'Кнопка 1', callback_data: '1' }],
+                  [{ text: 'Кнопка 2', callback_data: 'data 2' }],
+                  [{ text: 'Кнопка 3', callback_data: 'text 3' }]
+                ]
+              })
         });
 }
 
