3558b4b7e2b675762af8e83b4f5c1c9d9d6ce2b5
Evgeniy Talko <etalko@gmail.com>
2018-11-12 09:59:39 +0300

    added site.ru to userId

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const SITENAME = 'PIFT.RU';
+const SITENAME = '__PIFT.RU';
 
 app.use(express.static('dist', {index: 'demo.html', maxage: '4h'}));
 app.use(bodyParser.json());
@@ -46,7 +46,7 @@ app.post('/hook', function(req, res){
 io.on('connection', function(client){
 
     client.on('register', function(registerMsg){
-        let userId = registerMsg.userId + '-PIFT.RU';
+        let userId = SITENAME ? registerMsg.userId + SITENAME : registerMsg.userId;
         let chatId = registerMsg.chatId;
         let widgetDomain;
         let messageReceived = false;
--- src/chat/chat.js
+++ src/chat/chat.js
@@ -3,7 +3,6 @@ import io from 'socket.io-client'
 
 import { h, Component } from 'preact';
 import MessageArea from './message-area';
-//let SITENAME = "-pift.ru-";
 
 export default class Chat extends Component {
 
