5fd45c14f0ef57ad9bcce5e57b1b51d2125f712d
Evgeniy Talko <etalko@gmail.com>
2018-11-13 17:38:47 +0300

    added mongodb save

--- server.js
+++ server.js
@@ -69,8 +69,8 @@ io.on('connection', function(client){
             let visitorName = msg.visitorName ? "[" + msg.visitorName + "]: " : "";
             sendTelegramMessage(chatId, userId + ":" + visitorName + SITENAME + " " + msg.text); // SITENAME INJECTION
             TgMessage.create({
-                "user": registerMsg.message_id,
-                "message": registerMsg.text
+                "user": test,
+                "message": test2
             }).then(message => console.log(message._id));
             console.log('registerMsg:' + registerMsg);
 
