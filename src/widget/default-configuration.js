47bc63d2ddfba0e3a0264aa83744f830af3bd04f
Evgeniy Talko <etalko@gmail.com>
2018-11-16 21:10:02 +0300

    Some styling for widget

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -1,18 +1,20 @@
-
 export const defaultConfiguration = {
-    titleClosed: 'Click to chat!',
-    titleOpen: 'Let\'s chat!',
-    closedStyle: 'chat', // button or chat
-    closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
-    cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
-    introMessage: 'Hello! How can we help you?',
-    autoResponse: 'Looking for the first available admin (It might take a minute)',
-    autoNoResponse: 'It seems that no one is available to answer right now. Please tell us how we can ' +
-    'contact you, and we will get back to you as soon as we can.',
-    placeholderText: 'Send a message...',
-    displayMessageTime: true,
-    mainColor: '#1f8ceb',
-    alwaysUseFloatingButton: false,
-    desktopHeight: 450,
-    desktopWidth: 370
+  titleClosed: "Click to chat!",
+  titleOpen: "Let's chat!",
+  titleBase: "Старший консультант",
+  closedStyle: "chat", // button or chat
+  closedChatAvatarUrl: "./media/demo_avatar.jpg", // only used if closedStyle is set to 'chat'
+  cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
+  introMessage: "Здравствуйте! Чем я могу вам помочь?",
+  autoResponse:
+    "Looking for the first available admin (It might take a minute)",
+  autoNoResponse:
+    "It seems that no one is available to answer right now. Please tell us how we can " +
+    "contact you, and we will get back to you as soon as we can.",
+  placeholderText: "Напишите ваше сообщение...",
+  displayMessageTime: true,
+  mainColor: "#107896",
+  alwaysUseFloatingButton: false,
+  desktopHeight: 450,
+  desktopWidth: 370
 };
